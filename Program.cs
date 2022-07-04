fe550c9f3044baacec0ab4cd53295a84dd0a28ec
azizburibaev <azizburibaev@yahoo.com>
2022-07-04 11:43:53 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myRec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int myNum = Convert.ToInt32(Console.ReadLine());
+            reMeth(myNum);
+            void reMeth(int num)
+            {
+                Console.WriteLine(num);
+                //int maxNum = num;
+                //if (num == maxNum)
+                if(num == 0)
+                {
+                    return;
+                }
+                else
+                {
+                     reMeth(num-1);
+
+                }
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
